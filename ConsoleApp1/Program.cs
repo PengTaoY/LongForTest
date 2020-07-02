05281f43fa3901290b20d9d7507ee3bf8c7be642
PengtaoY <1197726544@qq.com>
2020-07-02 21:54:27 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestModel testModel = new TestModel
+            {
+                Name = "张三",
+                Id = 3
+            };
+            Console.WriteLine(nameof(testModel));
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+        class TestModel
+        {
+            public string Name { get; set; }
+
+            public int Id { get; set; }
+        }
+    }
+}
