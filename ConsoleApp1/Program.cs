c92fd4ea4fe65d940b74a320b2162aa09f06a069
PengtaoY <1197726544@qq.com>
2020-07-08 17:01:57 +0800

    添加图片操作的Demo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Web;
 
 namespace ConsoleApp1
 {
@@ -7,15 +12,128 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            TestModel testModel = new TestModel
-            {
-                Name = "张三",
-                Id = 3
-            };
-            Console.WriteLine(nameof(testModel));
-
+            HttpDownloadFile(@"http://risk.longfor.sit/file/download/noauth/138aa91f-d6dc-4e16-b056-756f6f1c69093", @"D:\123", true);
+            HttpDownloadFile(@"http://risk.longfor.sit/file/download/noauth/02972f75-ded6-4061-9b1a-f2e4c8cd38b5", @"D:\123", true);
+            //HttpDownloadFile(@"https://www.cnblogs.com/images/logo_small.gif", @"D:\123", true);
+            //HttpDownloadFile(@"http://risk.longfor.sit/file/download/noauth/f33b1abf-b17d-4819-97ac-94f01481a52a", @"D:\123", true);
 
             Console.WriteLine("Hello World!");
+        }
+
+        public static void HttpDownloadFile(string url, string path, bool overwrite)
+        {
+            // 设置参数
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            //发送请求并获取相应回应数据
+            HttpWebResponse response;
+            try
+            {
+                response = request.GetResponse() as HttpWebResponse;
+            }
+            catch (WebException ex)
+            {
+                response = ex.Response as HttpWebResponse;
+            }
+
+            //获取文件名
+            string conentDisposition = response.Headers["Content-Disposition"];
+            string fileName;
+            if (string.IsNullOrEmpty(conentDisposition))
+                fileName = response.ResponseUri.Segments[response.ResponseUri.Segments.Length - 1];
+            else
+                fileName = conentDisposition.Remove(0, conentDisposition.IndexOf("UTF-8''") + 7);
+
+            fileName = HttpUtility.UrlDecode(fileName);
+
+            #region 第二种方式
+            //ContentDisposition contentDisposition = new ContentDisposition(conentDisposition);
+            //string sss = contentDisposition.FileName;
+            #endregion
+
+
+            MemoryStream memoryStream = StreamToMemoryStream(response.GetResponseStream());
+
+
+
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                long totalLength = response.ContentLength;
+                //创建本地文件写入流
+                using (Stream stream = new FileStream(Path.Combine(path, fileName), overwrite ? FileMode.Create : FileMode.CreateNew))
+                {
+                    byte[] bArr = new byte[1024];
+                    int size;
+                    while ((size = responseStream.Read(bArr, 0, bArr.Length)) > 0)
+                    {
+                        stream.Write(bArr, 0, size);
+                    }
+                }
+            }
+
+            DeleteFile(path, Path.Combine(path, fileName));
+
+        }
+
+        private static MemoryStream StreamToMemoryStream(Stream stream)
+        {
+            MemoryStream ms = new MemoryStream();
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int sz = stream.Read(buffer, 0, 1024);
+                if (sz == 0) break;
+                ms.Write(buffer, 0, sz);
+            }
+            ms.Position = 0;
+            return ms;
+        }
+
+        public static void DeleteFile(string directoryPath, string fileName)
+        {
+            try
+            {
+                File.Delete(fileName);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"{directoryPath}下没有该文件");
+                throw;
+            }
+            
+            //for (int i = 0; i < Directory.GetFiles(directoryPath).ToList().Count; i++)
+            //{
+            //    if (Directory.GetFiles(directoryPath)[i] == fileName)
+            //    {
+            //        File.Delete(fileName);
+            //    }
+            //}
+        }
+
+        public static void Httpdownload()
+        {
+            string url = @"http://risk.longfor.sit/file/download/noauth/138aa91f-d6dc-4e16-b056-756f6f1c6909";
+            string newFileName = Guid.NewGuid().ToString();
+            var httpClient = new System.Net.Http.HttpClient();
+
+            var t = httpClient.GetByteArrayAsync(url);
+            t.Wait();
+
+
+            string fileExtension = (Path.GetExtension(url) ?? string.Empty).ToLower();
+            if (string.IsNullOrWhiteSpace(newFileName))
+            {
+                newFileName = Path.GetFileName(url);
+            }
+            else
+            {
+                //文件名+文件后缀名
+                newFileName = newFileName + fileExtension;
+            }
+            //return File(t.Result, "application/octet-stream", newFileName);
+
+            File.WriteAllBytes($"D:\\123\\{newFileName}", t.Result);
         }
 
         class TestModel
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            //Create a stream for the file
+            Stream stream = null;
+
+            //This controls how many bytes to read at a time and send to the client
+            int bytesToRead = 10000;
+
+            // Buffer to read bytes in chunk size specified above
+            byte[] buffer = new Byte[bytesToRead];
+
+            string url = @"https://www.baidu.com/s?wd=%e9%ab%98%e8%80%83&sa=ire_dl_gh_logo&rsv_dl=igh_logo_pc";
+
+            // The number of bytes read
+            try
+            {
+                //Create a WebRequest to get the file
+                HttpWebRequest fileReq = (HttpWebRequest)HttpWebRequest.Create(url);
+
+                //Create a response for this request
+                HttpWebResponse fileResp = (HttpWebResponse)fileReq.GetResponse();
+
+                if (fileReq.ContentLength > 0)
+                    fileResp.ContentLength = fileReq.ContentLength;
+
+                //Get the Stream returned from the response
+                stream = fileResp.GetResponseStream();
+
+                // prepare the response to the client. resp is the client Response
+                var resp = HttpContext.Current.Response;
+
+                //Indicate the type of data being sent
+                resp.ContentType = "application/octet-stream";
+
+                //Name the file 
+                resp.AddHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+                resp.AddHeader("Content-Length", fileResp.ContentLength.ToString());
+
+                int length;
+                do
+                {
+                    // Verify that the client is connected.
+                    if (resp.IsClientConnected)
+                    {
+                        // Read data into the buffer.
+                        length = stream.Read(buffer, 0, bytesToRead);
+
+                        // and write it out to the response's output stream
+                        resp.OutputStream.Write(buffer, 0, length);
+
+                        // Flush the data
+                        resp.Flush();
+
+                        //Clear the buffer
+                        buffer = new Byte[bytesToRead];
+                    }
+                    else
+                    {
+                        // cancel the download if client has disconnected
+                        length = -1;
+                    }
+                } while (length > 0); //Repeat until no data is read
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    //Close the input stream
+                    stream.Close();
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+
+            return "OK";
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
